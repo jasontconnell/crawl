5fa4bfbf625ef233158a468681c36caced1d1fe8
Jason Connell <jconnell@delphicdigital.com>
2018-08-25 01:22:03 -0400

    Add mod. Go fmt

--- main.go
+++ main.go
@@ -1,17 +1,17 @@
 package main
 
 import (
-    "fmt"
-    "runtime"
-    "time"
-    "sync"
-    "encoding/json"
-    "os"
-    "net/http"
-    "net/url"
-    "io/ioutil"
-    "regexp"
-    "strings"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"os"
+	"regexp"
+	"runtime"
+	"strings"
+	"sync"
+	"time"
 )
 
 var hrefRegex *regexp.Regexp = regexp.MustCompile(`(href|src)="(.*?)"`)
@@ -20,199 +20,198 @@ var gatheredUrls map[string]string = make(map[string]string)
 type Headers map[string]string
 
 type Site struct {
-    Root string                 `json:"root"`
-    VirtualPaths []string       `json:"virtualPaths"`
-    Headers Headers             `json:"headers"`
-    ErrorFileName string            `json:"errorFile"`
-    ErrorFile *os.File
-    AllUrlsFileName string      `json:"allUrlsFile"`
-    AllUrlsFile *os.File
+	Root            string   `json:"root"`
+	VirtualPaths    []string `json:"virtualPaths"`
+	Headers         Headers  `json:"headers"`
+	ErrorFileName   string   `json:"errorFile"`
+	ErrorFile       *os.File
+	AllUrlsFileName string `json:"allUrlsFile"`
+	AllUrlsFile     *os.File
 }
 
 type Link struct {
-    Referrer string
-    Url string
+	Referrer string
+	Url      string
 }
 
 type ContentResponse struct {
-    Link Link
-    Code int
-    Content string
+	Link    Link
+	Code    int
+	Content string
 }
 
 func Parse(site Site, referrer, content string) (urls chan Link) {
-    var wg sync.WaitGroup
-    wg.Add(1)
-    urls = make(chan Link)
+	var wg sync.WaitGroup
+	wg.Add(1)
+	urls = make(chan Link)
 
-    go func(){
-        defer close(urls)
-        defer wg.Done()
+	go func() {
+		defer close(urls)
+		defer wg.Done()
 
-        matches := hrefRegex.FindAllStringSubmatch(content, -1)
-        for _,m := range matches {
-            href := strings.Trim(m[2], " ")
-            add := (href != "" && strings.HasPrefix(href, "/") && !strings.HasPrefix(href, "//") && !strings.HasPrefix(href, "#") && !strings.HasPrefix(href, "mailto:") && !strings.HasPrefix(href, "javascript")) || strings.HasPrefix(href, site.Root)
-            if strings.HasPrefix(href, "/") {
-                href = site.Root + href
-            }
+		matches := hrefRegex.FindAllStringSubmatch(content, -1)
+		for _, m := range matches {
+			href := strings.Trim(m[2], " ")
+			add := (href != "" && strings.HasPrefix(href, "/") && !strings.HasPrefix(href, "//") && !strings.HasPrefix(href, "#") && !strings.HasPrefix(href, "mailto:") && !strings.HasPrefix(href, "javascript")) || strings.HasPrefix(href, site.Root)
+			if strings.HasPrefix(href, "/") {
+				href = site.Root + href
+			}
 
-            _,contains := gatheredUrls[href]
-            _,contains2 := gatheredUrls[href + "/"]
-            _,contains3 := gatheredUrls[strings.TrimSuffix(href, "/")]
-            contains = contains || contains2 || contains3
+			_, contains := gatheredUrls[href]
+			_, contains2 := gatheredUrls[href+"/"]
+			_, contains3 := gatheredUrls[strings.TrimSuffix(href, "/")]
+			contains = contains || contains2 || contains3
 
-            if !contains {
-                gatheredUrls[href] = href
-            }
+			if !contains {
+				gatheredUrls[href] = href
+			}
 
-            if add && !contains {
-                urls <- Link{ Referrer: referrer, Url: href }
-            }
-        }
-    }()
+			if add && !contains {
+				urls <- Link{Referrer: referrer, Url: href}
+			}
+		}
+	}()
 
-    return
+	return
 }
 
-func init(){
-    runtime.GOMAXPROCS(runtime.NumCPU())
+func init() {
+	runtime.GOMAXPROCS(runtime.NumCPU())
 }
 
-func main(){
-    startTime := time.Now()
+func main() {
+	startTime := time.Now()
 
-    if file, err := os.OpenFile("entrypoints.json", os.O_RDONLY, os.ModePerm); err == nil {
-        enc := json.NewDecoder(file)
+	if file, err := os.OpenFile("entrypoints.json", os.O_RDONLY, os.ModePerm); err == nil {
+		enc := json.NewDecoder(file)
 
-        var site Site
-        jserr := enc.Decode(&site)
+		var site Site
+		jserr := enc.Decode(&site)
 
-        site.ErrorFile,_ = os.Create(site.ErrorFileName)
-        site.ErrorFile.Chmod(os.ModeAppend)
-        site.ErrorFile.WriteString("Url, Referrer, Code\n")
+		site.ErrorFile, _ = os.Create(site.ErrorFileName)
+		site.ErrorFile.Chmod(os.ModeAppend)
+		site.ErrorFile.WriteString("Url, Referrer, Code\n")
 
-        site.AllUrlsFile,_ = os.Create(site.AllUrlsFileName)
-        site.AllUrlsFile.Chmod(os.ModeAppend)
-        site.AllUrlsFile.WriteString("Url, Referrer\n")
-        site.AllUrlsFile.WriteString(site.Root + ",[Root]\n")
+		site.AllUrlsFile, _ = os.Create(site.AllUrlsFileName)
+		site.AllUrlsFile.Chmod(os.ModeAppend)
+		site.AllUrlsFile.WriteString("Url, Referrer\n")
+		site.AllUrlsFile.WriteString(site.Root + ",[Root]\n")
 
+		if jserr == nil {
+			urls := getStartUrlList(site)
+			churl := make(chan Link, 15000)
+			chcresp := make(chan ContentResponse, 300)
+			chproc := make(chan ContentResponse, 15000)
+			cherrs := make(chan ContentResponse, 300)
+			errCount := 0
+			finished := make(chan bool)
 
-        if jserr == nil {
-            urls := getStartUrlList(site)
-            churl := make(chan Link, 15000)
-            chcresp := make(chan ContentResponse, 300)
-            chproc := make(chan ContentResponse, 15000)
-            cherrs := make(chan ContentResponse, 300)
-            errCount := 0
-            finished := make(chan bool)
-            
-            for i := 0; i < 8; i++ {
-                go process(site, churl, chcresp, chproc, cherrs, &errCount, finished)
-            }
-         
-            for _,url := range urls {
-                gatheredUrls[url] = url
-                churl <- Link{ Referrer: site.Root, Url: url}
-            }
+			for i := 0; i < 8; i++ {
+				go process(site, churl, chcresp, chproc, cherrs, &errCount, finished)
+			}
 
-            <-finished
-        } else {
-            fmt.Println(jserr)
-        }
-    }
-    
-    fmt.Println("\n\ndone. time", time.Since(startTime))
+			for _, url := range urls {
+				gatheredUrls[url] = url
+				churl <- Link{Referrer: site.Root, Url: url}
+			}
+
+			<-finished
+		} else {
+			fmt.Println(jserr)
+		}
+	}
+
+	fmt.Println("\n\ndone. time", time.Since(startTime))
 }
 
 func getStartUrlList(site Site) (list []string) {
-    list = []string{}
-    list = append(list, site.Root)
-    for _,i := range site.VirtualPaths {
-        list = append(list, site.Root + i)
-    }
-    return
+	list = []string{}
+	list = append(list, site.Root)
+	for _, i := range site.VirtualPaths {
+		list = append(list, site.Root+i)
+	}
+	return
 }
 
-func process(site Site, urls chan Link, content, processed, errors chan ContentResponse, errCount *int, finished chan bool){
-    for {
-        select {
-        case url := <-urls:
-            
-            fmt.Printf("\rRoot: %v  Url queue: %d. Content queue: %d. Processed: %d. Errors: %d\t", site.Root, len(urls), len(content), len(processed), *errCount)
-            ch := getUrlContents(site, url)
-            content <- <-ch
+func process(site Site, urls chan Link, content, processed, errors chan ContentResponse, errCount *int, finished chan bool) {
+	for {
+		select {
+		case url := <-urls:
 
-        case cresp := <-content:
-            processed <- cresp
-            fmt.Printf("\rRoot: %v  Url queue: %d. Content queue: %d. Processed: %d. Errors: %d\t", site.Root, len(urls), len(content), len(processed), *errCount)
+			fmt.Printf("\rRoot: %v  Url queue: %d. Content queue: %d. Processed: %d. Errors: %d\t", site.Root, len(urls), len(content), len(processed), *errCount)
+			ch := getUrlContents(site, url)
+			content <- <-ch
 
-            if cresp.Code == 200 {
-                hrefs := Parse(site, cresp.Link.Url, cresp.Content)
-                for href := range hrefs {
-                    urls <- href
-                    // unique urls.
-                    site.AllUrlsFile.WriteString(href.Url + "," + href.Referrer + "\n")
+		case cresp := <-content:
+			processed <- cresp
+			fmt.Printf("\rRoot: %v  Url queue: %d. Content queue: %d. Processed: %d. Errors: %d\t", site.Root, len(urls), len(content), len(processed), *errCount)
 
-                }
-            } else if cresp.Code >= 400 {
-                errors <- cresp
-            }
+			if cresp.Code == 200 {
+				hrefs := Parse(site, cresp.Link.Url, cresp.Content)
+				for href := range hrefs {
+					urls <- href
+					// unique urls.
+					site.AllUrlsFile.WriteString(href.Url + "," + href.Referrer + "\n")
 
-            allDone := len(processed) > 1 && len(urls) == 0 && len(content) == 0
-            if allDone { 
-                finished <- true
-            }
-        case err := <-errors:
-            (*errCount)++
-            site.ErrorFile.WriteString(fmt.Sprintf("%v, %v, %v\n", err.Link.Url, err.Link.Referrer, err.Code))
-        } 
-    }
+				}
+			} else if cresp.Code >= 400 {
+				errors <- cresp
+			}
+
+			allDone := len(processed) > 1 && len(urls) == 0 && len(content) == 0
+			if allDone {
+				finished <- true
+			}
+		case err := <-errors:
+			(*errCount)++
+			site.ErrorFile.WriteString(fmt.Sprintf("%v, %v, %v\n", err.Link.Url, err.Link.Referrer, err.Code))
+		}
+	}
 }
 
-func getUrlContents(site Site, link Link) (chresp chan ContentResponse){
-    var wg sync.WaitGroup
-    wg.Add(1)
-    chresp = make(chan ContentResponse)
+func getUrlContents(site Site, link Link) (chresp chan ContentResponse) {
+	var wg sync.WaitGroup
+	wg.Add(1)
+	chresp = make(chan ContentResponse)
 
-    go func(){
-        defer close(chresp)
-        defer wg.Done()
+	go func() {
+		defer close(chresp)
+		defer wg.Done()
 
-        uri,parseErr := url.Parse(link.Url)
-        if parseErr != nil {
-            panic(parseErr)
-        }
+		uri, parseErr := url.Parse(link.Url)
+		if parseErr != nil {
+			panic(parseErr)
+		}
 
-        headers := make(map[string][]string)
-        for k,v := range site.Headers {
-            headers[k] = []string{ v }
-        }
+		headers := make(map[string][]string)
+		for k, v := range site.Headers {
+			headers[k] = []string{v}
+		}
 
-        req := &http.Request{ Method: "GET", URL: uri, Header: headers }
-        client := &http.Client{
-            CheckRedirect: func(req *http.Request, via []*http.Request) error {
-                if strings.Contains(site.Root, req.URL.Host){
-                    return nil
-                }
-                return http.ErrUseLastResponse
-            },
-        }
+		req := &http.Request{Method: "GET", URL: uri, Header: headers}
+		client := &http.Client{
+			CheckRedirect: func(req *http.Request, via []*http.Request) error {
+				if strings.Contains(site.Root, req.URL.Host) {
+					return nil
+				}
+				return http.ErrUseLastResponse
+			},
+		}
 
-        resp,err := client.Do(req)
-        defer resp.Body.Close()
+		resp, err := client.Do(req)
+		defer resp.Body.Close()
 
-        if err == nil {
-            if contents, err := ioutil.ReadAll(resp.Body); err == nil {
-                text := string(contents)
-                chresp <- ContentResponse { Link: link, Code: resp.StatusCode, Content: text }
-            } else {
-                fmt.Println(err)
-            }
-        } else {
-            fmt.Println(err)
-        }
-    }()
-    
-    return
+		if err == nil {
+			if contents, err := ioutil.ReadAll(resp.Body); err == nil {
+				text := string(contents)
+				chresp <- ContentResponse{Link: link, Code: resp.StatusCode, Content: text}
+			} else {
+				fmt.Println(err)
+			}
+		} else {
+			fmt.Println(err)
+		}
+	}()
+
+	return
 }
